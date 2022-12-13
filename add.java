fd6a635f6aa71c1d42a2173d4cecbcd2fd01dde4
guna <gunavardhan799@gmail.com>
2022-12-13 11:59:43 +0530

    add program

---
+++ add.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class add
+{
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        int b;
+        int c;
+        System.out.println("enter a value");
+        a=sc.nextInt();
+        System.out.println("enter b value");
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("sum of two numbers is :"+c);
+        }
+
+}
