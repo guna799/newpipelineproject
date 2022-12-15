d2130539ffc6712b40f26c1b864bae64ac703e2f
guna <gunavardhan799@gmail.com>
2022-12-15 13:10:21 +0530

    guna commits

---
+++ scholl.java
@@ -0,0 +1,8 @@
+public class scholl
+{
+    public static void main (String [] args)
+    {
+        System.out.println("this is project to deploy in git");
+        System.out.println("this project wiill be build in jenkis with mail notification");
+    }
+}
