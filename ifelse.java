d7c33140da360adb4c203159ade5423961f2f8d8
guna <gunavardhan799@gmail.com>
2022-12-16 11:30:43 +0530

    new file is added

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public class ifelse
+{
+    public static void main(String [] args)
+    {
+       int a=23;
+       int b=78;
+        if(a>b)
+        {
+            System.out.println("a is largest number");
+        }
+        else
+        {
+            System.out.println("b is largest number");
+        }
+    }
+}
